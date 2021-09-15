bb3daaaee9d2d5609c03662f8187e243a4833101
Artem Shavkunov <shavkunov.a@seointellect.ru>
2021-09-15 12:07:46 +0300

    json api vue article component

--- resources/views/app/article/show.blade.php
+++ resources/views/app/article/show.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.app')
 @section('content')
     <div id="app">
-        <div class="row mt-5">
-            <div class="col-12 p-3">
-                <img src="{{$article->img}}" class="border rounded mx-auto d-block" alt="...">
-                <h5 class="mt-5">{{$article->title}}</h5>
-                <p>
-                    @foreach($article->tags as $tag)
-                        @if($loop->last)
-                            <span class="tag">{{$tag->label}}</span>
-                        @else
-                            <span class="tag">{{$tag->label}} |</span>
-                        @endif
-                    @endforeach
-                </p>
-                <p class="card-text">{{$article->body}}</p>
-                <p>Published:  <i>{{$article->createdAtForHumans()}}</i></p>
-            </div>
-        </div>
+        <article-component></article-component>
+{{--        <div class="row mt-5">--}}
+{{--            <div class="col-12 p-3">--}}
+{{--                --}}
+{{--                <img src="{{$article->img}}" class="border rounded mx-auto d-block" alt="...">--}}
+{{--                <h5 class="mt-5">{{$article->title}}</h5>--}}
+{{--                <p>--}}
+{{--                    @foreach($article->tags as $tag)--}}
+{{--                        @if($loop->last)--}}
+{{--                            <span class="tag">{{$tag->label}}</span>--}}
+{{--                        @else--}}
+{{--                            <span class="tag">{{$tag->label}} |</span>--}}
+{{--                        @endif--}}
+{{--                    @endforeach--}}
+{{--                </p>--}}
+{{--                <p class="card-text">{{$article->body}}</p>--}}
+{{--                <p>Published:  <i>{{$article->createdAtForHumans()}}</i></p>--}}
+{{--                <div class="mt-3">--}}
+{{--                    <span class="badge bg-primary">{{ $article->state->likes }}<i class="far fa-thumbs-up"></i></span>--}}
+{{--                    <span class="badge bg-danger">{{ $article->state->views }}<i class="far fa-eye"></i></span>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
         <hr>
         <div class="row">
             <form action="">
@@ -50,5 +56,5 @@
 
 @endsection
 @section('vue')
-
+    <script src="{{ mix('/js/app.js') }}"></script>
 @endsection
