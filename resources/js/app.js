2d317a4348953608cdfa615958eee05f8f584462
Artem Shavkunov <shavkunov.a@seointellect.ru>
2021-09-25 10:06:21 +0300

    store separation & finish

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,6 @@ window.Vue = require('vue').default;
 
 import store from './store'
 
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('article-component', require('./components/ArticleComponent.vue').default);
 Vue.component('views-component', require('./components/ViewsComponent.vue').default);
 Vue.component('likes-component', require('./components/LikesComponent.vue').default);
